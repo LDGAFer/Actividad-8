cdb54c7d288d19504a3c4a681abbdb7d88fd06c2
LDGAFer <90427903+LDGAFer@users.noreply.github.com>
2022-07-13 06:26:58 -0500

    Add files via upload

---
+++ Actividad_Poker/src/main/java/com/mycompany/actividad_poker/Deck.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividad_poker;
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Deck {
+
+    private List<Card> card;
+    private Integer numCard = 52;
+
+    public List<Card> getCard() {
+        return card;
+    }
+
+    public void setCard(List<Card> card) {
+        this.card = card;
+    }
+
+    public Integer getNumCard() {
+        return numCard;
+    }
+
+    public void setNumCard(Integer numCard) {
+        this.numCard = numCard;
+    }
+
+    public String mezclar() throws Exception{
+        if (getCard().size()==0){
+            throw new Exception("Se han agotado las cartas. Saliendo...");
+        }
+
+        Collections.shuffle(card);
+        return "mezclando...";
+    }
+
+    public void removeCard(int pos){
+        this.card.remove(pos);
+    }
+
+    public void head() throws Exception{
+        if (getCard().size()==0){
+            throw new Exception("Se han agotado las cartas. Saliendo...");
+        }
+
+        Card card= getCard().get(0);
+        System.out.println("{" +card.getPalo() +"},{" +card.getColor() +"},{" +card.getValor() +"}");
+        removeCard(0);
+        System.out.println("Quedan:" +getCard().size());
+    }
+
+    public void pick() throws Exception{
+        if (getCard().size()==0){
+            throw new Exception("Se han agotado las cartas. Saliendo...");
+        }
+
+        Random numRandom = new Random();
+        int num = numRandom.nextInt(getCard().size());
+        Card card = getCard().get(num);
+        System.out.println("{" +card.getPalo() +"},{" +card.getColor() +"},{" +card.getValor() +"}");
+        removeCard(num);
+        System.out.println("Quedan:" +getCard().size());
+    }
+
+    public void hand() throws Exception{
+        if (getCard().size()==0){
+            throw new Exception("Se han agotado las cartas. Saliendo...");
+        }
+
+        for (int i=0; i<5; i++){
+            Random numRandom = new Random();
+            int num = numRandom.nextInt(getCard().size());
+            Card card = getCard().get(num);
+            System.out.println("{" +card.getPalo() +"},{" +card.getColor() +"},{" +card.getValor() +"}");
+            removeCard(num);
+        }
+        System.out.println("Quedan:" +getCard().size());
+    }
+
+    public void imprimir(){
+        for (int n=0; n<getCard().size(); n++){
+            Card card = getCard().get(n);
+            System.out.println("{" +card.getPalo() +"},{" +card.getColor() +"},{" +card.getValor() +"}");
+        }
+    }
+}
---
+++ Actividad_Poker/src/main/java/com/mycompany/actividad_poker/Main.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.actividad_poker;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        String[] palo = {"Pica", "Corazon", "Diamante", "Trebol"};
+        String[] valor = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        Integer cont = 1;
+        int opcion;
+        boolean exit=true;
+        Deck deck = new Deck();
+        deck.setCard(new ArrayList<Card>());
+        for (int i = 0; i < palo.length; i++) {
+            for (int j = 0; j < valor.length; j++) {
+                Card card = new Card(palo[i], getColor(palo[i]), valor[j]);
+                deck.getCard().add(card);
+            }
+        }
+        do{
+            try {
+            opcion=showMenu(deck);
+
+                            switch (opcion) {
+                    case 1:
+                        try {
+                            System.out.println("Mezclar Deck\n" + deck.mezclar());
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                            exit=false;
+                        }
+                        break;
+
+                    case 2:
+                        try {
+                            System.out.println("\nObtener la primer carta en la lista:");
+                            deck.head();
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                            exit=false;
+                        }
+                        break;
+
+                    case 3:
+                        try {
+                            System.out.println("\nCarta al azar:");
+                            deck.pick();
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                            exit=false;
+                        }
+                        break;
+
+                    case 4:
+                        try {
+                            System.out.println("\nArreglo de cartas:");
+                            deck.hand();
+                        } catch (Exception e){
+                            System.out.println(e.getMessage());
+                            exit=false;
+                        }
+                        break;
+
+                    case 0:
+                        System.out.println("Saliendo...");
+                        exit=false;
+                        break;
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        } while (exit);
+    }
+    
+    public static int showMenu(Deck deck) throws Exception{
+        Scanner texto = new Scanner(System.in);
+        int eleccion;
+
+        System.out.println("\n¡Bienvenido a Poker!");
+        System.out.println("La cantidad de cartas en el deck es de: " + deck.getCard().size()+"\n");
+
+        System.out.println("Selecciona una opción:");
+        System.out.println("1 Mezclar deck.");
+        System.out.println("2 Sacar una carta.");
+        System.out.println("3 Elegir una carta al azar");
+        System.out.println("4 Genera una mano de 5 cartas");
+        System.out.println("0 Salir");
+        eleccion = texto.nextInt();
+
+        if ((eleccion<0)||(eleccion>4)){
+            throw new Exception("Opción no válida. Ingresa un número del 1 al 4.");
+        }
+        return eleccion;
+    }
+
+    public static String getColor(String palo) {
+        if (palo.toLowerCase().equals("pica") || palo.toLowerCase().equals("trebol")) {
+            return "negro";
+        }
+        if (palo.toLowerCase().equals("corazon") || palo.toLowerCase().equals("diamante")) {
+            return "rojo";
+        }
+        return null;
+    }
+}
